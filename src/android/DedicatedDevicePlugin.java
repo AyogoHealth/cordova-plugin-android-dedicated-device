791decabe53e35737ec1a74a93e02d4fece669eb
Darryl Pogue <darryl@ayogo.com>
2018-09-26 16:07:55 -0700

    Initial commit of Android plugin

---
+++ src/android/DedicatedDevicePlugin.java
@@ -0,0 +1,110 @@
+/**
+ * Copyright 2018 Ayogo Health Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ayogo.cordova.dedicateddevice;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.admin.DevicePolicyManager;
+import android.app.admin.SystemUpdatePolicy;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.UserManager;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.LOG;
+
+
+// This is *heavily* inspired by Google's COSU Sample App code:
+// https://codelabs.developers.google.com/codelabs/cosu/index.html
+public class DedicatedDevicePlugin extends CordovaPlugin {
+    private static final String TAG = "DedicatedDevicePlugin";
+
+    private ComponentName mActivityComponentName;
+    private ComponentName mAdminComponentName;
+    private DevicePolicyManager mDevicePolicyManager;
+    private PackageManager mPackageManager;
+
+
+    @Override
+    protected void pluginInitialize() {
+        LOG.v(TAG, "Initializing");
+
+        Activity _act = cordova.getActivity();
+
+        mPackageManager = _act.getPackageManager();
+        mDevicePolicyManager = (DevicePolicyManager)_act.getSystemService(Context.DEVICE_POLICY_SERVICE);
+        mAdminComponentName = DeviceAdminReceiver.getComponentName(_act);
+        mActivityComponentName = new ComponentName(_act.getApplicationContext(), _act.getClass());
+
+        if (mDevicePolicyManager.isDeviceOwnerApp(_act.getApplicationContext().getPackageName())) {
+            setDefaultPolicies();
+        } else {
+            LOG.w(TAG, "App is NOT a device owner!");
+        }
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Activity _act = cordova.getActivity();
+
+        if (mDevicePolicyManager.isLockTaskPermitted(_act.getApplicationContext().getPackageName())) {
+            LOG.v(TAG, "Task Lock is permitted, enabling...");
+
+            mPackageManager.setComponentEnabledSetting(mActivityComponentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+
+            ActivityManager am = (ActivityManager)_act.getSystemService(Context.ACTIVITY_SERVICE);
+
+            if (am.getLockTaskModeState() == ActivityManager.LOCK_TASK_MODE_NONE) {
+                _act.startLockTask();
+            }
+        } else {
+            LOG.w(TAG, "Task Lock is NOT permitted!");
+        }
+    }
+
+
+    private void setDefaultPolicies() {
+        // set user restrictions
+        mDevicePolicyManager.addUserRestriction(mAdminComponentName, UserManager.DISALLOW_SAFE_BOOT);
+        mDevicePolicyManager.addUserRestriction(mAdminComponentName, UserManager.DISALLOW_FACTORY_RESET);
+        mDevicePolicyManager.addUserRestriction(mAdminComponentName, UserManager.DISALLOW_ADD_USER);
+        mDevicePolicyManager.addUserRestriction(mAdminComponentName, UserManager.DISALLOW_MOUNT_PHYSICAL_MEDIA);
+        mDevicePolicyManager.addUserRestriction(mAdminComponentName, UserManager.DISALLOW_ADJUST_VOLUME);
+
+        // disable keyguard and status bar
+        mDevicePolicyManager.setKeyguardDisabled(mAdminComponentName, true);
+        mDevicePolicyManager.setStatusBarDisabled(mAdminComponentName, true);
+
+        // set system update policy
+        mDevicePolicyManager.setSystemUpdatePolicy(mAdminComponentName, SystemUpdatePolicy.createWindowedInstallPolicy(60, 120));
+
+        // set the activity as a lock task package
+        mDevicePolicyManager.setLockTaskPackages(mAdminComponentName, new String[] { cordova.getActivity().getPackageName() });
+
+        // set the activity as a home intent receiver so that it's started on reboot
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_MAIN);
+        intentFilter.addCategory(Intent.CATEGORY_HOME);
+        intentFilter.addCategory(Intent.CATEGORY_DEFAULT);
+
+        mDevicePolicyManager.addPersistentPreferredActivity(mAdminComponentName, intentFilter, mActivityComponentName);
+    }
+}
---
+++ src/android/DeviceAdminReceiver.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2018 Ayogo Health Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ayogo.cordova.dedicateddevice;
+
+import android.content.ComponentName;
+import android.content.Context;
+
+/**
+ * Handles events related to the managed profile.
+ */
+public class DeviceAdminReceiver extends android.app.admin.DeviceAdminReceiver {
+    private static final String TAG = "DeviceAdminReceiver";
+
+    /**
+     * @param context The context of the application.
+     * @return The component name of this component in the given context.
+     */
+    public static ComponentName getComponentName(Context context) {
+        return new ComponentName(context.getApplicationContext(), DeviceAdminReceiver.class);
+    }
+}
